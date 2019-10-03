ed5f840c000be058e9c9ca3160833b5bcd6b683c
Beppe Raymaekers <beppe.raymaekers@gmail.com>
2019-10-03 11:11:42 +0200

    alignment of sh and batch scripts

--- bin/init.bat
+++ bin/init.bat
@@ -3,17 +3,17 @@
 REM Set Environment
 REM ~dp0 has \ at end already
 SET ROOT_DIR=%~dp0..\..
-REM echo ROOT_DIR: %ROOT_DIR%
 
 IF "%PENTAHO_ENV%"=="" (SET PENTAHO_ENV=config-pdi-local)
+
+ECHO *** ROOT_DIR: "%ROOT_DIR%" ***
 ECHO Running with "%PENTAHO_ENV%" environment settings
 
 REM Load Environment Configuration
-REM FOR /F "usebackq delims=" %%x IN ("%ROOT_DIR%\%PROJECT_NAME%-configuration\%PENTAHO_ENV%\env.conf") DO (SET %%x)
 FOR /F "usebackq delims=" %%x IN ("%BASE_CONFIG_DIR%\env.windows.conf") DO SET %%x
 
 REM Set Additional Variables
 IF "%KETTLE_CLIENT_DIR%"=="" (SET KETTLE_CLIENT_DIR=%USERPROFILE%\%PENTAHO_HOME%\data-integration)
-SET KETTLE_HOME=%ROOT_DIR%\%PROJECT_NAME%-configuration\%PENTAHO_ENV%
-SET KETTLE_META_HOME=%ROOT_DIR%\%PROJECT_NAME%-configuration\%PENTAHO_ENV%
-SET OPT="-DPENTAHO_METASTORE_FOLDER=%KETTLE_META_HOME%" "-DROOT_DIR=%ROOT_DIR%" "-DPROJECT_ENV=config-pdi-local" "-DPROJECT_NAME=%PROJECT_NAME%"
+SET KETTLE_HOME=%BASE_CONFIG_DIR%
+SET KETTLE_META_HOME=%BASE_CONFIG_DIR%
+SET OPT="-DPENTAHO_METASTORE_FOLDER=%KETTLE_META_HOME%" "-DROOT_DIR=%ROOT_DIR%" "-DPROJECT_ENV=%PROJECT_ENV%" "-DPROJECT_NAME=%PROJECT_NAME%"
--- bin/init.sh
+++ bin/init.sh
@@ -4,17 +4,20 @@
 ROOT_DIR="$PWD"/../..
 PROJECT_ENV="${PROJECT_ENV:-config-pdi-local}"
 
+echo "***" ROOT_DIR: "$ROOT_DIR" "***"
 echo "***" Running with "$PROJECT_ENV" environment settings "***"
 
 # Load Environment Configuration
-. $ROOT_DIR/$PROJECT_NAME-configuration/$PROJECT_ENV/env.conf
+#. $ROOT_DIR/$PROJECT_NAME-configuration/$PROJECT_ENV/env.conf
+. $BASE_CONFIG_DIR/env.linux.conf
 echo "***" Setting PENTAHO_HOME to $PENTAHO_HOME "***"
 
 # Set Additional Variables
 export KETTLE_CLIENT_DIR="${KETTLE_CLIENT_DIR:-$PENTAHO_HOME/design-tools/data-integration}"
-export KETTLE_HOME=$ROOT_DIR/$PROJECT_NAME-configuration/$PROJECT_ENV
-export KETTLE_META_HOME=$ROOT_DIR/$PROJECT_NAME-configuration/$PROJECT_ENV
+export KETTLE_HOME=$BASE_CONFIG_DIR
+export KETTLE_META_HOME=$BASE_CONFIG_DIR
 export OPT="$OPT -DPENTAHO_METASTORE_FOLDER=$KETTLE_META_HOME"
+#-DROOT_DIR=$ROOT_DIR -DPROJECT_ENV=$PROJECT_ENV -DPROJECT_NAME=$PROJECT_NAME
 
 echo "***" Setting KETTLE_CLIENT_DIR to $KETTLE_CLIENT_DIR "***"
 echo "***" Setting KETTLE_HOME to $KETTLE_HOME "***"
